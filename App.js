6d0bd5a8312f34b27ff8fe8154e199af59d752dc
Gonchi <gonzaloreyesllauca@gmail.com>
2026-04-19 17:23:17 -0700

    First commit :)

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  Button,
+  ToastAndroid,
+  Platform,
+  Alert,
+} from "react-native";
+
+function showToast() {
+  if (Platform.OS === "android") {
+    ToastAndroid.show("Hello World", ToastAndroid.SHORT);
+  } else {
+    Alert.alert("Hi there!");
+  }
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Press me" onPress={showToast} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
